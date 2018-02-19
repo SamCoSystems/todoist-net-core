7f687c10ab2e430ec0459a8a2d3f8da2682441e0
Sam Ferree <sferree567@gmail.com>
2018-02-18 23:34:59 -0500

    the Project.cs file was missing a class, added private setters for read only props

--- TodoistNetCore/Models/Project.cs
+++ TodoistNetCore/Models/Project.cs
@@ -2,9 +2,12 @@ using System
 
 namesapce TodoistNetCore.Models
 {
-	public int Id { get; set; }
-	public string Name { get; set; }
-	public int Order { get; set; }
-	public int Indent { get; set; }
-	public int CommentCount { get; set; }
+	public class Project
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int Order { get; private set; }
+		public int Indent { get; private set; }
+		public int CommentCount { get; set; }
+	}
 }
